0e1377bfe31e91ba84e740868eba336ec2a401d6
joel <joel.barba@blueface.com>
2019-10-15 18:03:02 +0100

    Bundling with zoneJs

--- src/app/hello-world/hello-world.component.ts
+++ src/app/hello-world/hello-world.component.ts
@@ -19,13 +19,13 @@ export class HelloWorldComponent implements OnInit {
     console.log('!', new Date());
     this.myVar += '!';
     if (this.myVar.length > 30) { this.myVar = 'hello world!'; }
-    this.cd.detectChanges();
+    // this.cd.detectChanges();
     // this.cd.markForCheck();
   };
 
   reset = () => {
     this.myVar = 'x';
-    this.cd.detectChanges();
+    // this.cd.detectChanges();
   }
 
 }
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import 'zone.js/dist/zone';
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
@@ -9,6 +10,7 @@ if (environment.production) {
 }
 
 platformBrowserDynamic()
-  .bootstrapModule(AppModule, { ngZone: 'noop' })
+  .bootstrapModule(AppModule)
+  // .bootstrapModule(AppModule, { ngZone: 'noop' })
   // .bootstrapModule(AppModule, { ngZone: (window as any).ngZone })
   // .catch(err => console.error(err));
